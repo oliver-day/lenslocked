cfd62f607296a0a5b6768a168505f18ed346a0f7
oliver-day <oliver.day.swe@gmail.com>
2024-07-21 14:17:00 -0700

    add 'pathHandler' func

--- main.go
+++ main.go
@@ -15,8 +15,13 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", pathHandler)
 	http.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("Server is running on port 3000")
