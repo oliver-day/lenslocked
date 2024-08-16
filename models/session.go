cb17b14045573d30becafbed63ff75e7627fc972
oliver-day <oliver.day.swe@gmail.com>
2024-08-16 15:14:28 -0700

    insert session record to db w/ SessionService.Create

--- models/session.go
+++ models/session.go
@@ -52,7 +52,15 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
-	// TODO: Store the session in the database
+	row := ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id, token_hash)
+		VALUES ($1, $2)
+		RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
 	return &session, nil
 }
 
