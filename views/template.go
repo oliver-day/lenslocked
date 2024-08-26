4fe515984d34442a6b941489fd415c93ce774516
oliver-day <oliver.day.swe@gmail.com>
2024-08-25 17:46:06 -0700

    add 'currentUser' template function

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/oliver-day/lenslocked/context"
+	"github.com/oliver-day/lenslocked/models"
 )
 
 func Must(t Template, err error) Template {
@@ -33,6 +35,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (*models.User, error) {
+				return nil, fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -58,6 +63,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
 			},
 		},
 	)
