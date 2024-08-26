fdfcf0b7eeccc50ca7ac87ac63e5ab54b5a936d9
oliver-day <oliver.day.swe@gmail.com>
2024-08-25 17:19:42 -0700

    integrate user middleware w/ main.go

--- main.go
+++ main.go
@@ -15,32 +15,21 @@ import (
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
-		templates.FS,
-		"home.gohtml", "tailwind.gohtml",
-	))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-
-	// Setup a database connection
+	// Step 1: Set up a database connection
 	cfg := models.DefaultPostgresConfig()
+	fmt.Println(cfg.String())
 	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Run goose db migrations
 	err = models.MigrateFS(db, migrations.FS, ".")
 	if err != nil {
 		panic(err)
 	}
 
-	// Setup model services
+	// Step 2: Set up services
 	userService := models.UserService{
 		DB: db,
 	}
@@ -48,7 +37,19 @@ func main() {
 		DB: db,
 	}
 
-	// Setup users controller
+	// Step 3: Set up middleware
+	umw := controllers.UserMiddleware{
+		SessionService: &sessionService,
+	}
+
+	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	csrfMw := csrf.Protect(
+		[]byte(csrfKey),
+		// TODO: Fix this before deploying
+		csrf.Secure(false),
+	)
+
+	// Step 4: Set up controllers
 	usersC := controllers.Users{
 		UserService:    &userService,
 		SessionService: &sessionService,
@@ -58,27 +59,36 @@ func main() {
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS, "signin.gohtml", "tailwind.gohtml"))
 
+	// Step 5: Set up router and routes
+	r := chi.NewRouter()
+	r.Use(csrfMw)
+	r.Use(umw.SetUser)
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"contact.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"faq.gohtml", "tailwind.gohtml",
+	))))
 	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
-
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
-	r.Post("/signout", usersC.ProcessSignOut)
-	r.Get("/users/me", usersC.CurrentUser)
-
+	r.With(umw.RequireUser).Post("/signout", usersC.ProcessSignOut)
+	r.Route("/users/me", func(r chi.Router) {
+		r.Use(umw.RequireUser)
+		r.Get("/", usersC.CurrentUser)
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
-	userMiddleware := controllers.UserMiddleware{
-		SessionService: &sessionService,
-	}
-	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	csrfMw := csrf.Protect(
-		[]byte(csrfKey),
-		// TODO: Fix this before deploying
-		csrf.Secure(false),
-	)
+	// Step 6: Start the server
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", csrfMw(userMiddleware.SetUser(r)))
+	http.ListenAndServe(":3000", r)
 }
