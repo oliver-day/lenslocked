513cd0f650b35c25812e162ac6a2553fc66fdb8d
oliver-day <oliver.day.swe@gmail.com>
2024-08-31 13:40:52 -0400

    add forgot pw template and endpoints

--- main.go
+++ main.go
@@ -58,6 +58,8 @@ func main() {
 		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS, "signin.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(
+		templates.FS, "forgot-pw.gohtml", "tailwind.gohtml"))
 
 	// Step 5: Set up router and routes
 	r := chi.NewRouter()
@@ -80,6 +82,8 @@ func main() {
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
 	r.With(umw.RequireUser).Post("/signout", usersC.ProcessSignOut)
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
