97fc368f8c6c5946e238ec12165aa023bc4ab050
oliver-day <oliver.day.swe@gmail.com>
2024-09-03 10:43:59 -0400

    add 'reset_pw' template

--- main.go
+++ main.go
@@ -115,6 +115,10 @@ func main() {
 		templates.FS, "forgot-pw.gohtml", "tailwind.gohtml"))
 	usersC.Templates.CheckYourEmail = views.Must(views.ParseFS(
 		templates.FS, "check-your-email.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ResetPassword = views.Must(views.ParseFS(
+		templates.FS,
+		"reset-pw.gohtml", "tailwind.gohtml",
+	))
 
 	// Step 6: Set up router and routes
 	r := chi.NewRouter()
@@ -139,6 +143,8 @@ func main() {
 	r.With(umw.RequireUser).Post("/signout", usersC.ProcessSignOut)
 	r.Get("/forgot-pw", usersC.ForgotPassword)
 	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
+	r.Get("/reset-pw", usersC.ResetPassword)
+	r.Post("/reset-pw", usersC.ProcessResetPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
