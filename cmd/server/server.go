2576581addee7b7f25c8c5a78b6ceb8632e4b687
oliver-day <oliver.day.swe@gmail.com>
2024-11-12 17:47:56 -0800

    setup '/oauth/{provider}/connect' route

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/csrf"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 
 	"github.com/oliver-day/lenslocked/controllers"
 	"github.com/oliver-day/lenslocked/migrations"
@@ -27,6 +28,7 @@ type config struct {
 	Server struct {
 		Address string
 	}
+	OAuthProviders map[string]*oauth2.Config
 }
 
 func loadEnvConfig() (config, error) {
@@ -60,6 +62,18 @@ func loadEnvConfig() (config, error) {
 	cfg.CSRF.Secure = os.Getenv("CSRF_SECURE") == "true"
 
 	cfg.Server.Address = os.Getenv("SERVER_ADDRESS")
+
+	cfg.OAuthProviders = make(map[string]*oauth2.Config)
+	dbxConfig := &oauth2.Config{
+		ClientID:     os.Getenv("DROPBOX_APP_ID"),
+		ClientSecret: os.Getenv("DROPBOX_APP_SECRET"),
+		Scopes:       []string{"files.metadata.read", "files.content.read"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/oauth2/authorize",
+			TokenURL: "https://api.dropboxapi.com/oauth2/token",
+		},
+	}
+	cfg.OAuthProviders["dropbox"] = dbxConfig
 
 	return cfg, nil
 }
@@ -150,6 +164,9 @@ func run(cfg config) error {
 		templates.FS,
 		"reset-pw.gohtml", "tailwind.gohtml",
 	))
+	oauthC := controllers.OAuth{
+		ProviderConfigs: cfg.OAuthProviders,
+	}
 
 	// Step 6: Set up router and routes
 	r := chi.NewRouter()
@@ -194,6 +211,10 @@ func run(cfg config) error {
 			r.Post("/{id}/images", galleriesC.UploadImage)
 			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
 		})
+	})
+	r.Route("/oauth/{provider}", func(r chi.Router) {
+		r.Use(umw.RequireUser)
+		r.Get("/connect", oauthC.Connect)
 	})
 	assetsHandler := http.FileServer(http.Dir("assets"))
 	r.Get("/assets/*", http.StripPrefix("/assets", assetsHandler).ServeHTTP)
